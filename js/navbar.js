17ecab68a7f0aedb7ba23f60ee14a2a923bac30c
katenpak <katenpak@gmail.com>
2018-08-12 17:19:09 -0700

    update status

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,4 +7,20 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+}
+*/
+
+alert("hi");
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("topnav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
